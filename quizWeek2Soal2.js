c7d782125059dae7c3505be779aa5bea18852b31
imamma17 <98438702+imamma17@users.noreply.github.com>
2022-01-30 11:34:07 +0700

    Delete quizWeek2Soal2.js

--- quizWeek2Soal2.js
+++
@@ -1,9 +0,0 @@
-function appendText(){
-    var text1 = "<p>Nama : Imam Muhammad Allif</p>"
-    var text2 = "<P>Alamat : Kapalo Koto</p>"
-    var text3 = "<P>Gender : Laki-laki</p>"
-    var text4 = "Asal daerah : Sumatra Barat</p>"
-    var text5 = "<P>Usia : 20 thn</p>"
-
-    $("body").append(text1,text2,text3,text4,text5)
-}
