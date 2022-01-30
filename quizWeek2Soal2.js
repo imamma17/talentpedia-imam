568657ffd1c0631bdfb7733dde6e946827452da0
imamma17 <imammallif1@gmail.com>
2022-01-30 11:22:21 +0700

    tugas minggu 2

---
+++ quizWeek2Soal2.js
@@ -0,0 +1,9 @@
+function appendText(){
+    var text1 = "<p>Nama : Imam Muhammad Allif</p>"
+    var text2 = "<P>Alamat : Kapalo Koto</p>"
+    var text3 = "<P>Gender : Laki-laki</p>"
+    var text4 = "Asal daerah : Sumatra Barat</p>"
+    var text5 = "<P>Usia : 20 thn</p>"
+
+    $("body").append(text1,text2,text3,text4,text5)
+}
