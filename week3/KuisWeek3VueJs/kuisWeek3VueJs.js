4fb0d2c243c645244eda5ec47e42ddd6e4fe639f
imamma17 <98438702+imamma17@users.noreply.github.com>
2022-02-06 19:14:02 +0700

    Week3

---
+++ week3/KuisWeek3VueJs/kuisWeek3VueJs.js
@@ -0,0 +1,13 @@
+var app= new Vue ({
+    el: "#app",
+    data: {
+        show:true,
+        namaSiswa:" ",
+        umurSiswa:" ",
+        orangTuaSiswa:" ",
+        tingkat:" ",
+        kelas:" ",
+        walikelas:" ",
+        batas:" "
+    }
+})
